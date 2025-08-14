00fe9f878c5b703523042dbaa229119e7d9469f6
Estevão Fernandes <estevaofernandes744@gmail.com>
2025-08-14 09:11:35 -0300

    new

--- introducaoAProgramacaoDeComputadores/ex004.c
+++ introducaoAProgramacaoDeComputadores/ex004.c
@@ -3,8 +3,7 @@
 int main(){
 
     int idade;
-    double altura;
-    char option;
+    float altura;
     char name[5];
 
     printf("Digite a sua idade: ");
@@ -14,10 +13,6 @@ int main(){
     printf("Digite a sua altura: ");
     scanf("%f", &altura);
     printf("A altura é: %.2f\n", altura);
-
-    printf("Digite a opção selecionada: ");
-    scanf(" %c", &option);
-    printf("A opção selecionada é: %c\n", option);
 
     printf("Digite o nome desejado: ");
     scanf("%s", name);
