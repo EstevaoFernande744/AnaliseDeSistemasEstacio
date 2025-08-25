859d442b933112f97406e2bcbd0580f87921369f
Estevão Fernandes <estevaofernandes744@gmail.com>
2025-08-25 09:27:03 -0300

    new projet

--- introducaoAProgramacaoDeComputadores/Challenges/xadrezChellenge.c
+++ introducaoAProgramacaoDeComputadores/Challenges/xadrezChellenge.c
@@ -295,6 +295,16 @@ int main(){
     
         case 4:
 
+            printf("\n");
+            printf("╔═════════════════════════════════════════╗\n");
+            printf("║  direção desejada para mover o Bispo:   ║\n");
+            printf("╠═════════════════════════════════════════╣\n");
+            printf("║  1 ▸ Para Diagonal Esquerda Superior;   ║\n");
+            printf("║  2 ▸ Para Diagonal Direita Superior;    ║\n");
+            printf("║  3 ▸ Para Diagonal Esquerda Inferior;   ║\n");
+            printf("║  4 ▸ Para Diagonal Direita Inferior;    ║\n");
+            printf("╚═════════════════════════════════════════╝\n");
+
             
             printf("Informe qual movimento quer fazer com o cavalo, sendo:\n");
             printf("1. L - Esquerda-Baixo;\n");
--- xadrezLogicInsane.c
+++ xadrezLogicInsane.c
@@ -1,61 +1,105 @@
 #include <stdio.h>
 
-void selecionarPecas(int tipo){
+void selecionarPecas(int tipo){ 
 
-   /*
-   
-    printf("Menu")
+}
 
-    switch (tipo){
+void indentificarBordas(int edgeDirection){
+
+}
+
+void moveHorse(int config){
+    switch (config){
         case 1:
-            
+            printf("Para Esquerda\n");
+            printf("Para Esquerda\n");
+            printf("Para Baixo\n");
             break;
-        
+        case 2:
+            printf("Para Esquerda\n");
+            printf("Para Esquerda\n");
+            printf("Para Cima\n");
+            break;
+        case 3:
+            printf("Para Cima\n");
+            printf("Para Cima\n");
+            printf("Para Esquerda\n");
+            break;
+        case 4:
+            printf("Para Cima\n");
+            printf("Para Cima\n");
+            printf("Para Direita\n");
+            break;
+        case 5:
+            printf("Para Direita\n");
+            printf("Para Direita\n");
+            printf("Para Cima\n");
+            break;
+        case 6:
+            printf("Para Direita\n");
+            printf("Para Direita\n");
+            printf("Para Baixo\n");
+            break;
+        case 7:
+            printf("Para Baixo\n");
+            printf("Para Baixo\n");
+            printf("Para Direita\n");
+            break;
+        case 8:
+            printf("Para Baixo\n");
+            printf("Para Baixo\n");
+            printf("Para Esquerda\n");
+            break;
         default:
+            printf("Opção Invalida, lembre-se de 1 a 8");
             break;
     }
-
-
-   */
 }
 
-void indentificarBordas(){
+void moveBishop(int squares, int direction, char edge[5], int edgeDirection){
 
-}
-
-void moveHorse(){
-
-}
-
-void moveBishop(){
+    if(strcmp(edge, "nao") == 0 || strcmp(edge, "n") == 0 || strcmp(edge, "no") == 0){
+        switch (direction){
+            case 1: for(int i = 1;i <= squares;i++){ printf("%d Movimento(s) para Diagonal Esquerda Superior;\n", i);} break;
+            case 2: for(int i = 1;i <= squares;i++){ printf("%d Movimento(s) para Diagonal Direita Superior;\n", i);} break;
+            case 3: for(int i = 1;i <= squares;i++){ printf("%d Movimento(s) para Diagonal Esquerda Inferior;\n", i);} break;
+            case 4: for(int i = 1;i <= squares;i++){ printf("%d Movimento(s) para Diagonal Direita Superior;\n", i);} break;
+            default: printf("Opção selecionada Invalida!\n"); break;}
+    } else if (strcmp(edge, "sim") == 0 || strcmp(edge, "s") == 0 || strcmp(edge, "yes") == 0){
+            indentificarBordas(edgeDirection);
+    } 
     
 }
 
-void moveTower(){
+void moveTower(int squares, int direction){
 
 }
 
-void moveQueen(){
+void moveQueen(int squares, int direction){
 
 }
 
-int main(){
+int main(int option){
 
+    do{
     int option;
 
-    printf("\n");
-    printf("╔════════════════════════════════════╗\n");
-    printf("║         MENU PRINCIPAL 🎮          ║\n");
-    printf("╠════════════════════════════════════╣\n");
-    printf("║  1 ▸ Exibir as Regras              ║\n");
-    printf("║  2 ▸ Iniciar o Jogo                ║\n");
-    printf("║  3 ▸ Placar                        ║\n");
-    printf("║  4 ▸ Créditos                      ║\n");
-    printf("║  5 ▸ Sair                          ║\n");
-    printf("╚════════════════════════════════════╝\n");
-    printf("\nDigite a opção desejada: ");
-    scanf("%d", &option);
-
+        printf("\n");
+        printf("╔════════════════════════════════════╗\n");
+        printf("║    Menu to the Pieces Move: 🎮     ║\n");
+        printf("╠════════════════════════════════════╣\n");
+        printf("║  1 ▸ Move the Horse;               ║\n");
+        printf("║  2 ▸ Move the Bishop;              ║\n");
+        printf("║  3 ▸ Move the Tower;               ║\n");
+        printf("║  4 ▸ Move the Queen;               ║\n");
+        printf("║  5 ▸ Leave game.                   ║\n");
+        printf("╚════════════════════════════════════╝\n");
+        printf("\nDigite a opção desejada: ");
+        scanf("%d", &option);
     
 
+
+    } while(option != 5);
+
+    return 0;
 }
